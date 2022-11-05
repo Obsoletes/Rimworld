8c01c551aa5348d58b0777b41d8d919257399807
Obsoletes <DevilZP@outlook.com>
2022-11-05 14:59:11 +0800

    添加设置地板

---
+++ Dev In Your Language Fix/Source/DevL10N.Fix/Patch/DebugActionsMapManagement/SetTerrainRect.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace DevL10N.Fix.Patch
+{
+	[HarmonyPatch(typeof(DebugActionsMapManagement), "SetTerrainRect")]
+	public static class DebugActionsMapManagement_SetTerrainRect
+	{
+		[HarmonyTranspiler]
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			return Patch.Transpiler(instructions);
+		}
+	}
+}
